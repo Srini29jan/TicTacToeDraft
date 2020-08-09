773f8758f5b8e3788ca8f4a40f112714e64da79d
Srinivas Subburaj <srinivassubburaj@testkata.com>
2020-08-09 23:32:46 +0530

    Refactored: Extracted TicTacToeGame object as a constant and used @Before annotation to create new object

--- src/test/java/com/bnpp/kata/TicTacToeGameTest.java
+++ src/test/java/com/bnpp/kata/TicTacToeGameTest.java
@@ -2,17 +2,25 @@ package com.bnpp.kata;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TicTacToeGameTest {
 
+	protected TicTacToeGame ticTacToeGame;
+
+	@Before
+	public void initializeTicTacToeGameObject() {
+		ticTacToeGame = new TicTacToeGame();
+	}
+
 	@Test
 	public void checkTicTacToeGameObjectNotNull() {
-		assertNotNull(new TicTacToeGame());
+		assertNotNull(ticTacToeGame);
 	}
 
 	@Test
 	public void checkIfTicTacToeGameBoardPositionZeroZeroIsInitializedToNullCharacter() {
-		assertEquals('\0', new TicTacToeGame().ticTacToeGameBoard[0][0]);
+		assertEquals('\0', ticTacToeGame.ticTacToeGameBoard[0][0]);
 	}
 }
