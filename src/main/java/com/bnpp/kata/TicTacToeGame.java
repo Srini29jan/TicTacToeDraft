a32842fcbc58b6b571f3e26d9b3ec34f253918bc
Srinivas Subburaj <srinivassubburaj@testkata.com>
2020-08-09 23:37:15 +0530

    Refactored: Moved the initialization part of ticTacToeGameBoard to constructor

--- src/main/java/com/bnpp/kata/TicTacToeGame.java
+++ src/main/java/com/bnpp/kata/TicTacToeGame.java
@@ -3,5 +3,9 @@ package com.bnpp.kata;
 public class TicTacToeGame {
 
 	private static final int THREE = 3;
-	protected char[][] ticTacToeGameBoard = new char[THREE][THREE];
+	protected char[][] ticTacToeGameBoard;
+
+	public TicTacToeGame() {
+		ticTacToeGameBoard = new char[THREE][THREE];
+	}
 }
