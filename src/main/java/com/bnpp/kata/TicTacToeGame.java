bb7f0a4a3c54582f5ebe44cc6fb36baf48229124
Srinivas Subburaj <srinivassubburaj@testkata.com>
2020-08-09 22:52:10 +0530

    Added: Test case to check if the newly created TicTacToeGameObject is not null

---
+++ src/main/java/com/bnpp/kata/TicTacToeGame.java
@@ -0,0 +1,5 @@
+package com.bnpp.kata;
+
+public class TicTacToeGame {
+
+}
---
+++ src/test/java/com/bnpp/kata/TicTacToeGameTest.java
@@ -0,0 +1,13 @@
+package com.bnpp.kata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TicTacToeGameTest {
+
+	@Test
+	public void checkTicTacToeGameObjectNotNull() {
+		assertNotNull(new TicTacToeGame());
+	}
+}
