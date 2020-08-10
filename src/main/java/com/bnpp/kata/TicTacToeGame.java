8fe0f458e28d407054cadc2e9a8a9004df63765e
Srinivas Subburaj <srinivassubburaj@testkata.com>
2020-08-10 15:47:55 +0530

    Refactored: Extracted constants

--- src/main/java/com/bnpp/kata/TicTacToeGame.java
+++ src/main/java/com/bnpp/kata/TicTacToeGame.java
@@ -4,13 +4,13 @@ import org.apache.log4j.Logger;
 
 public class TicTacToeGame {
 
+	private static final String NO_WINNER_MESSAGE = "The game ended without a winner";
+	private static final int NINE = 9;
 	private static final String TRY_AGAIN_MESSAGE = "Read the message above and try again with valid inputs";
 	private static final String WINNER_MESSAGE = "Winner of the game is Player ";
 	private static final int FOUR = 4;
-	private static final char CHAR_NULL = '\0';
 	private GameBoard gameBoard;
 	private final Logger LOGGER = Logger.getLogger(TicTacToeGame.class);
-	private char winner = '\0';
 	private static final char NO_WIN = 'N';
 
 	public TicTacToeGame() {
@@ -30,13 +30,14 @@ public class TicTacToeGame {
 	}
 
 	public boolean isGameOver() {
+		char winner;
 		if (gameBoard.numberOfCellsFilled > FOUR) {
 			winner = gameBoard.checkForResultOfTheGame();
 			if (winner != NO_WIN) {
 				LOGGER.info(WINNER_MESSAGE+ winner);
 				return true;
-			} else if (gameBoard.numberOfCellsFilled == 9) {
-				LOGGER.info("The game ended without a winner");
+			} else if (gameBoard.numberOfCellsFilled == NINE) {
+				LOGGER.info(NO_WINNER_MESSAGE);
 				return true;
 			}
 		}
