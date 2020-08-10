1bd81a5427eeb56f7c005453bf579355cf4bd25a
Srinivas Subburaj <srinivassubburaj@testkata.com>
2020-08-10 16:31:52 +0530

    Refactored: Extracted constants and added logger

--- src/main/java/com/bnpp/kata/PlayTicTacToeGame.java
+++ src/main/java/com/bnpp/kata/PlayTicTacToeGame.java
@@ -2,13 +2,19 @@ package com.bnpp.kata;
 
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 public class PlayTicTacToeGame {
 
+	private static final char CHAR_NULL = '\0';
+	private static final int NEGATIVE_ONE = -1;
+	private static final Logger LOGGER = Logger.getLogger(PlayTicTacToeGame.class);
+	
 	public static void main(String[] args) {
 		Scanner keyboardInput = new Scanner(System.in);
-		int row = -1;
-		int column = -1;
-		char winner = '\0';
+		int row = NEGATIVE_ONE;
+		int column = NEGATIVE_ONE;
+		char winner = CHAR_NULL;
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
 		while(!ticTacToeGame.isGameOver()) {
 			if(keyboardInput.hasNextInt()) {
@@ -20,7 +26,7 @@ public class PlayTicTacToeGame {
 			ticTacToeGame.drawOnBoard(row, column);
 			winner = ticTacToeGame.getGameBoard().checkForResultOfTheGame();
 		}
-		System.out.println("winner: "+ winner);
+		LOGGER.debug("winner: "+ winner);
 		keyboardInput.close();
 	}
 
